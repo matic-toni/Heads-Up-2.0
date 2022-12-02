4403bf38502efc051c34b1ed5e67e2b9581496d1
matic-toni <tonimatic8@gmail.com>
2022-12-02 17:47:20 +0100

    Added first version of the Final HeadsUp App

---
+++ Final App/app/src/main/java/org/butterbrot/floe/distribat/SplashActivity.java
@@ -0,0 +1,35 @@
+package org.butterbrot.floe.distribat;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView imageView = findViewById(R.id.HeadsUp);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        imageView.startAnimation(animation);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+
+    }
+}
